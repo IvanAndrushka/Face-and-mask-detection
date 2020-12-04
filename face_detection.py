2fed5c562be2c8e78cbd1b8a6161bb453435e798
Ivan <ivanandrushka@gmail.com>
2020-12-04 17:19:10 +0000

    Face and face mask detection project

---
+++ face_detection.py
@@ -0,0 +1,15 @@
+import cv2
+from mtcnn.mtcnn import MTCNN
+
+detector = MTCNN()
+img = cv2.imread('example2.jpg')
+faces = detector.detect_faces(img) # result
+
+#to draw faces on image
+for result in faces:
+    x, y, w, h = result['box']
+    x1, y1 = x + w, y + h
+    cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 2)
+
+cv2.imshow("Image",img)
+cv2.waitKey(0)
